95aacaff94545bdb8ce0a12287cf674937312ad3
denri88 <clatx88@gmail.com>
2017-03-07 22:38:23 -0600

    re did code for practice and added comments

--- js_simpleQuiz.js
+++ js_simpleQuiz.js
@@ -1,46 +1,59 @@
+// Array of questions and answers below
+
 var questions = [
-  ['How many states are in the United States?', 50],
-  ['How many continents are there?', 7],
-  ['How many legs does an insect have?', 6]
+["How many countries are there in the US? ", 50],
+["How many continents are there?" , 7],
+["How many legs do insects have?", 6]
 ];
+
+
+//variables needed for correct and incorrect arrays, prompt response, and correct answer counter
+
 var correctAnswers = 0;
-var incorrect = [];
 var correct = [];
-var question;
-var answer;
+var incorrect = [];
 var response;
+var answer;
+var question;
 var html;
 
-function buildList(arr){
-  var orderList = '<ol>';
+
+// Display the correct answers in an ordered list
+
+function orderList(arr){
+  var buildList = '<ol>';
   for(var i = 0; i < arr.length; i+=1){
-  orderList += '<li>' + arr[i] + '</li>';
+    buildList += '<li> ' + arr[i] + ' </li>';
+    
   }
-  orderList += '</ol>';
-  return orderList;
+  buildList+= '</ol>';
+  return buildList;
 }
+
+// Test the correct response
+
+for (var i = 0; i < questions.length; i+=1){
+  question = questions[i][0];
+  answer = questions[i][1];
+  response = prompt(questions[i][0]);
+  response = parseInt(response);
+  if(response == answer){
+    correctAnswers +=1;
+    correct.push(question);
+  } else {
+  incorrect.push(question);
+  }
+}
+
 
 function print(message) {
 var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
 }
 
-for (var i = 0; i < questions.length; i += 1) {
-  question = questions[i][0];
-  answer = questions[i][1];
-  response = prompt(question);
-  response = parseInt(response);
-  if (response === answer) {
-    correctAnswers +=1;
-    correct.push(question);
-  } else {
-    incorrect.push(question);
-  }
-}
-
-html = "You got " + correctAnswers + " question(s) right.";
-html += '<h2> Your Correct Answers: </h2>';
-html+= buildList(correct);
-html += '<h2> Your Incorrect Answers: </h2>';
-html+= buildList(incorrect);
+html = 'You have ' + correctAnswers + ' correct answers';
+html += '<h2> Your correct answers: </h2>';
+html += orderList(correct);
+html += '<h2> Your wrong answers: </h2>';
+html += orderList(incorrect);
 print(html);
